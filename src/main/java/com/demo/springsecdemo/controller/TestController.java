ce12af5bc28c0ee5ad312f93df59dd52341f91fa
cpatil2 <cpatil2@binghamton.edu>
2020-11-01 22:41:06 +0530

    Adding greeting apis for user and admin to check authorisation configured for apis

--- src/main/java/com/demo/springsecdemo/controller/TestController.java
+++ src/main/java/com/demo/springsecdemo/controller/TestController.java
@@ -3,13 +3,25 @@ package com.demo.springsecdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Test APIs for authentication and authorization demo
+ */
 @RestController
 public class TestController {
-	
-	@GetMapping("/somereadapi")
-	public String getString() {
-		
-		
+
+	@GetMapping("/all")
+	public String getGreetingForAll() {
 		return "Hello world";
 	}
+
+	@GetMapping("/user")
+	public String getGreetingForUser() {
+		return "Hello User";
+	}
+
+	@GetMapping("/admin")
+	public String getGreeingForAdmin() {
+		return "Hello Admin";
+	}
+
 }
