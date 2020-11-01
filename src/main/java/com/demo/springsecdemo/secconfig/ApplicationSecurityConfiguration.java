4046290f6013dd4684e09ab802c1818638bccf0f
cpatil2 <cpatil2@binghamton.edu>
2020-11-01 20:11:36 +0530

    adding in-memory hardcoded users through enablewebsecurity configuration

---
+++ src/main/java/com/demo/springsecdemo/secconfig/ApplicationSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.demo.springsecdemo.secconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/*
+ * customized security configurations for this webapp
+ */
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("Rahul")
+			.password("Dravid")
+			.roles("USER")
+			.and()
+			.withUser("Sachin")
+			.password("Tendulkar")
+			.roles("USER");
+	}
+	
+	@Bean
+	protected PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
